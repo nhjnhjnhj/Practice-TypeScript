8c82d9186b5470bc3956fe29ef5a09ef56cdbc7d
nhjnhjnhj <g2172095@tcu.ac.jp>
2024-04-22 02:00:54 +0900

    Hello!*3

---
+++ greeter.ts
@@ -0,0 +1,16 @@
+const main = () => {
+    console.log('Hello!');
+    console.log("Hello!");
+    console.log("Hello!");
+};
+
+main();
+
+// function greeter(person){
+//     return "Hello, " + person;
+// }
+
+// let user = "Shunta Higashino";
+
+// // document.body.textContent = greeter(user);
+// console.log(greeter(user));
