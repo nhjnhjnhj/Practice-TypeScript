937a4d08607f9adda001068b2d3cb0563ef33f90
nhjnhjnhj <g2172095@tcu.ac.jp>
2024-04-22 01:57:27 +0900

    Helloworld

---
+++ Helloworld.ts
@@ -0,0 +1,11 @@
+
+const HW = () =>{
+
+    type Helloworld = string
+    //type test = expect<equal<Helloworld, string>>
+
+    let message: Helloworld = "Hello, world";
+    console.log(message);
+};
+
+HW();
